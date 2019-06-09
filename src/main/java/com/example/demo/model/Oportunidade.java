b146b2abab8564e5212aee1d592edead5ef767ff
Allef Sousa <allefsousa_1@hotmail.com>
2019-06-09 18:39:24 -0300

    Classe de modelo adicionada

---
+++ src/main/java/com/example/demo/model/Oportunidade.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Oportunidade {
+    private Long id;
+    private String nomeProcpecto;
+    private  String descricao;
+    private  BigDecimal valor;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeProcpecto() {
+        return nomeProcpecto;
+    }
+
+    public void setNomeProcpecto(String nomeProcpecto) {
+        this.nomeProcpecto = nomeProcpecto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Oportunidade that = (Oportunidade) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
